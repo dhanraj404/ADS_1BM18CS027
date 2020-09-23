1fb8b2f6a9640a9ff74169118c993565fb4dea6d
dhanraj404 <53806611+dhanraj404@users.noreply.github.com>
2020-09-23 15:48:29 +0530

    Update XOR_LinkedList.cpp

--- Day2/XOR_LinkedList.cpp
+++ Day2/XOR_LinkedList.cpp
@@ -3,8 +3,8 @@ using namespace std;
 
 struct Node {
   int val;
-  Node* npx;
-  Node(int val) : val(val), npx(NULL) {}
+  Node* link;
+  Node(int val) : val(val), link(NULL) {}
 };
 
 Node* XOR(Node* prev, Node* next) {
@@ -14,15 +14,15 @@ Node* XOR(Node* prev, Node* next) {
 Node* insertEnd(Node* head, int val) {
   if (!head) 
     return new Node(val);
-  Node* prev = NULL, *curr = head, *next = XOR(prev, curr->npx);
+  Node* prev = NULL, *curr = head, *next = XOR(prev, curr->link);
   while (next) {
     prev = curr;
     curr = next;
-    next = XOR(prev, curr->npx);
+    next = XOR(prev, curr->link);
   }
   next = new Node(val);
-  next->npx = XOR(curr, NULL);
-  curr->npx = XOR(curr->npx, next);
+  next->link = XOR(curr, NULL);
+  curr->link = XOR(curr->link, next);
   return head;
 }
 
@@ -30,16 +30,38 @@ Node* insertBeg(Node* head, int val) {
   if (!head) 
     return new Node(val);
   Node* node = new Node(val);
-  head->npx = XOR(head->npx, node);
-  node->npx = XOR(NULL, head);
+  head->link = XOR(head->link, node);
+  node->link = XOR(NULL, head);
   return node;
 }
 
+Node* deleteBeg(Node* head){
+    if(!head){
+        cout<<"The List is Empty";
+        return head;
+    }
+    else
+    {
+        Node* curr = head;
+        head = XOR(curr->link, NULL);
+        return head;
+    }   
+}
+
+Node* deleteEnd(Node* head){
+    if(!head){
+        cout<<"The List is Empty";
+        return head;
+    }
+    else{
+
+    }
+}
 void printList(Node* curr) {
   Node* prev = NULL;
   while (curr) {
     cout << curr->val << ' ';
-    Node* next = XOR(curr->npx, prev);
+    Node* next = XOR(curr->link, prev);
     prev = curr, curr = next;
   } 
   cout << endl;
@@ -63,6 +85,9 @@ int main() {
                     printList(head);
                     break;
             case 3: exit(0);
+                    break;
+            case 4:head = deleteBeg(head);
+                break;
         }
     }while(1);
     return 0;
